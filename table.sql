80985d2070677e7664e2d68d742f0f91f2669d43
Raphael <matosramosraphael@gmail.com>
2023-06-26 21:30:54 -0500

    3822

---
+++ table.sql
@@ -0,0 +1,19 @@
+CREATE TABLE user(
+    id int primary key AUTO_INCREMENT,
+    name varchar(250),
+    contactNumber varchar(20),
+    email varchar(50),
+    password varchar(250),
+    status varchar(20),
+    role varchar(20),
+    UNIQUE (email)
+);
+INSERT INTO user (
+        name,
+        contactNumber,
+        email,
+        password,
+        status,
+        role
+    )
+VALUES ('Admin', '123123123', 'admin@gmail.com', 'admin','true');
