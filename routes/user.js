3a7d728a724f230265548238a30be3b41c3d3448
Raphael <matosramosraphael@gmail.com>
2023-06-29 18:33:11 -0500

    1:31:05

--- routes/user.js
+++ routes/user.js
@@ -3,6 +3,7 @@ const connection = require('../connection');
 const router = express.Router();
 
 const jwt = require('jsonwebtoken');
+const nodemailer = require('nodemailer');
 require('dotenv').config();
 
 
@@ -43,6 +44,79 @@ router.post('/login', (req, res) => {
             res.status(200).json({ token: acessToken })
         }
     })
+})
+
+var transporter = nodemailer.createTransport({
+    service: 'smtp.office365.com',
+    port: 587,
+    secure: false,
+    auth: {
+        user: process.env.EMAIL,
+        pass: process.env.PASSWORD
+    }
+})
+
+router.post('/forgotPassword', (req, res) => {
+    const user = req.body;
+    query = "SELECT email,password FROM user WHERE email=?";
+    connection.query(query, [user.email], (err, results) => {
+        if (!err) {
+            if (results.length <= 0) {
+                return res.status(200).json({ message: "Password sent successfully to your email.." });
+            } else {
+                var mailOptions = {
+                    from: process.env.EMAIL,
+                    to: results[0].email,
+                    subject: "Password by Cafe Magnament System",
+                    html: '<p><b>Your Login details for Cafe Magnament System</b><br>Email:</br>' + results[0].email + '<br><b>Password: </br>' + results[0].password + '<br><a href="http://localhost:8080/user/login">Click here to login</a></p>'
+                };
+                transporter.sendMail(mailOptions, function(error, info) {
+                    if (error) {
+                        console.log(error);
+                    } else {
+                        console.log('Email sent: ' + info.response);
+                    }
+                });
+                return res.status(200).json({ message: "Password sent successfully to your email. " });
+            }
+        } else {
+            return res.status(500).json(err);
+        }
+    })
+})
+
+router.get('/get', (req, res) => {
+    var query = "SELECT id,name,email,contactNumber,status FROM user WHERE role='user'";
+    connection.query(query, (err, results) => {
+        if (!err) {
+            return res.status(200).json(results);
+        } else {
+            return res.status(500).json(err);
+        }
+    })
+})
+
+router.patch('/update', (req, res) => {
+    let user = req.body;
+    var query = "UPDATE user SET status=? WHERE id = ?";
+    connection.query(query, [user.status, user.id], (err, results) => {
+        if (!err) {
+            if (results.affecedRows == 0) {
+                return res.status(404).json({ message: "User id does not exist" });
+            }
+            return res.status(200).json({ message: "User Updated Successfully" })
+        } else {
+            return res.results(500).json(err);
+        }
+    })
+})
+
+router.get('/checkToken', (req, res) => {
+    return res.status(200).json({ message: "true" });
+})
+
+router.post('/changePassword', (req, res) => {
+    //const
 })
 
 module.exports = router;
